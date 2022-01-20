b6612cb391ef3c1833061283b3a3e1286b72b924
Kidami13 <ulyanaboo1407@gmail.com>
2022-01-20 11:18:22 +0200

    BE

---
+++ LEss_1/main.py
@@ -0,0 +1,19 @@
+
+var1 = 1
+var2 = 'be'
+var3 = False
+var4 = 1.0
+var5 = ('be', 'be2','be3')
+var6 = ['be', 'be2','be3']
+print(var1)
+print(type(var1))
+print(var2)
+print(type(var2))
+print(var3)
+print(type(var3))
+print(var4)
+print(type(var4))
+print(var5)
+print(type(var5))
+print(var6)
+print(type(var6))
